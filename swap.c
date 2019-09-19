3a03c303c03ce126c0841c93fd9d4272c94fc673
Earl Francis Valdehuesa <efvaldehuesa@gmail.com>
2019-09-19 16:42:11 +0800

    Added swap.c

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int x = 31;
+  int y = 45;
+  printf("Values before swapping: x=%d, y=%d\n", x, y);
+
+  int temp = x;
+  x = y;
+  y = temp;
+
+  printf("Values after swapping: x=%d, y=%d\n", x, y);
+  printf("Added a new line at the end.");
+}
+
